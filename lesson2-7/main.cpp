8ee88245dc97161a2d56b0dfb95cfba9c57e2bc4
chongshengzhujue <2351410978@qq.com>
2016-12-29 22:31:35 +0800

    增加了冒泡排序的实现

---
+++ lesson2-7/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <algorithm>
+#include "SortTestHelper.h"
+
+//—°‘Ò≈≈–Ú
+template<typename T>
+void selectSort(T arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int minIndex = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[minIndex])
+				minIndex = j;
+		}
+
+		std::swap(arr[i], arr[minIndex]);
+	}
+}
+
+//≤Â»Î≈≈–Ú
+template<typename T>
+void insertSort(T arr[], int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = i; j > 0; j--)
+		{
+			if (arr[j] < arr[j - 1])
+				swap(arr[j], arr[j - 1]);
+			else
+				break;
+		}
+	}
+}
+
+//≤Â»Î≈≈–ÚΩ¯Ω◊
+template<typename T>
+void insertSortAdvance(T arr[], int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		T temp = arr[i];
+		int j;
+		for (j = i; j > 0 && temp < arr[j - 1]; j--)
+			arr[j] = arr[j - 1];
+		arr[j] = temp;
+	}
+}
+
+//√∞≈›≈≈–Ú
+
+template<typename T>
+void bubbleSort(T arr[], int n)
+{
+	int temp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(arr[j], arr[j + 1]);
+			}
+
+		}
+	}
+}
+
+int main()
+{
+	int n = 10000;
+	int* arr = SortTestHelper::createIntArr(n, 10, n);
+	int* arrInsert = SortTestHelper::copyArr(arr, n);
+	int* arrBubble = SortTestHelper::copyArr(arr, n);
+
+	SortTestHelper::testSort("selection sort", selectSort, arr, n);
+	SortTestHelper::testSort("insert sort advance", insertSortAdvance, arrInsert, n);
+	SortTestHelper::testSort("bubble sort", bubbleSort, arrBubble, n);
+	//SortTestHelper::printArray(arr, n);
+
+
+	delete[] arr;
+
+	int a = 0;
+	std::cin >> a;
+}
